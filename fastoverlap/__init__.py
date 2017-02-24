c21b111a99716a79c78eb442111dc75b6b166231
Matthew Griffiths <mg542@logan>
2017-02-24 16:11:22 +0000

    Modifying setup.py to allow compilation of fortran modules

--- fastoverlap/__init__.py
+++ fastoverlap/__init__.py
@@ -1,5 +1,11 @@
 
 
-from .periodicAlignment import PeriodicAlign
+
 from .sphericalAlignment import SphericalHarmonicAlign, SphericalAlign
 from .soft import SOFT
+
+try:
+    import fastoverlap_f
+    from .periodicAlignment import PeriodicAlignFortran as PeriodicAlign
+except ImportError:
+    from .periodicAlignment import PeriodicAlign
