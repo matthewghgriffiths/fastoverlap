87d7376a18970d236f14053c364bbfff8bb92f3e
Matthew Griffiths <matthewghgriffiths@gmail.com>
2017-05-24 12:26:55 +0100

    Removing references to kdtree2.f90 in bnbalign.f90

--- fastoverlap/bnbAlignment.py
+++ fastoverlap/bnbAlignment.py
@@ -19,7 +19,7 @@ from utils import angle_axis2mat, mat2angle_axis, PriorityQueueHeap
 import f90
 if f90.have_fortran:
     from f90 import libbnb
-     
+
 class BranchandBoundMaster(object):
     def __init__(self, initialNodes=[], rtol=5e-2, atol=1e-3):
         self.queue = PriorityQueueHeap()
@@ -37,15 +37,15 @@ class BranchandBoundMaster(object):
                 self.best = node
             if node.lowerbound < self.best.upperbound:
                 node.addtoQueue(self.queue)
-        
+
     def run(self, niter=10000, force=False, iprint=0):
         while(not self.queue.empty()):
             node = self.queue.get()[-1]
             self.nodes += 1
-            if (node.lowerbound < 
+            if (node.lowerbound <
                 self.best.upperbound - self.rtol*self.best.upperbound - self.atol):
                 self.record.append((
-                    self.nodes, self.ncalc, self.queue.qsize(), self.quickcount, node.rot, node.width, 
+                    self.nodes, self.ncalc, self.queue.qsize(), self.quickcount, node.rot, node.width,
                     node.lowerbound, node.upperbound, self.best.upperbound))
                 if iprint:
                     if self.nodes%iprint == 0:
@@ -64,30 +64,30 @@ class BranchandBoundMaster(object):
                 if self.nodes > niter:
                     break
         return self.best
-        
-    def __call__(self, pos1, pos2, invert=True, niter=np.inf, 
+
+    def __call__(self, pos1, pos2, invert=True, niter=np.inf,
                  extra_inputs=None, force=False, iprint=0):
-                     
+
         pos1 = np.array(pos1)
         pos2 = np.array(pos2)
         cm1 = pos1.mean(0)[None,:]
         cm2 = pos2.mean(0)[None,:]
         pos1 -= cm1
         pos2 -= cm2
-        
+
         rot = np.zeros(3)
 
         r1 = norm(pos1, axis=1)
         r2 = norm(pos2, axis=1)
-    
+
         r1tr2 = (r1[:,None]*r2[None,:])
         r1pr2 = r1[:,None]**2 + r2[None,:]**2
         pos1tree = cKDTree(pos1, 10)
-        
+
         startNode = BranchNodeCluster(rot, 2*np.pi, pos1, pos2,
                                r1, r2, r1tr2, r1pr2, pos1tree)
         startNode.calcbounds(force=True)
-        
+
         inputs = [startNode]
         if invert:
             inputs.append(BranchNodeCluster(rot, 2*np.pi, pos1, -pos2,
@@ -95,26 +95,26 @@ class BranchandBoundMaster(object):
             inputs[-1].calcbounds(force=True)
         if extra_inputs is not None:
             inputs.extend(extra_inputs)
-            
+
         self.nodes=0
         self.__init__(inputs, self.rtol, self.atol)
         self.run(niter, force=force, iprint=iprint)
-        
+
         bestNode = self.best
         return bestNode.upperbound, pos1+cm1, bestNode.rpos2+cm1
-        
+
     def plot(self):
         n, ncalc, qsize, quick, vs, widths, lower, upper, best = \
             map(np.array, zip(*self.record))
-        
+
         fig, axes = plt.subplots(3,1, sharex=True)
-        
+
         axes[0].plot(n, best)
         axes[0].plot(n, lower)
         axes[0].scatter(n, upper, marker='+')
-        
+
         bestNode = self.best
-        
+
         axes[1].plot(n, -np.ones_like(n)*pi, lw=1, c='k')
         axes[1].scatter(n, vs[:,0], marker='+', c='r')
         axes[1].plot(n, np.ones_like(n)*bestNode.rot[0], lw=2, c='r')
@@ -127,16 +127,16 @@ class BranchandBoundMaster(object):
         axes[1].plot(n, np.ones_like(n)*5*pi, lw=1, c='k')
         axes[1].plot(n, widths + 5*np.pi)
         axes[1].plot(n, np.ones_like(n)*5*np.pi)
-        
+
         axes[2].plot(n, ncalc)
         axes[2].plot(n, qsize)
         axes[2].plot(n, quick)
-        
+
         axes[0].set_xlim(min(n)-10, max(n)+10)
         fig.tight_layout(h_pad=0.1)
         return fig, axes
-        
-        
+
+
 class BranchNode(object):
     def addtoQueue(self, queue):
         queue.put((self.lowerbound, self))
@@ -144,8 +144,8 @@ class BranchNode(object):
         raise NotImplementedError
     def calcbounds(self, bestupper=np.inf):
         raise NotImplementedError
-    
-        
+
+
 class BranchNodeBulk(BranchNode):
     branchvects = np.array(list(product([1,-1],repeat=3)))
     def __init__(self, disp, width, pos1, pos2, boxvec, perm):
@@ -154,17 +154,17 @@ class BranchNodeBulk(BranchNode):
         self.pos1, self.pos2 = pos1, pos2
         self.boxvec = boxvec
         self.perm = perm
-        
+
     def branch(self):
         newdisps = self.branchvects*self.width*0.25
-        newnodes = [BranchNodeBulk(disp, self.width*0.5, self.pos1, self.pos2, 
+        newnodes = [BranchNodeBulk(disp, self.width*0.5, self.pos1, self.pos2,
                                    self.boxvec, self.perm)
                     for disp in newdisps]
         return newnodes
-        
+
     def calcbounds(self, bestupper=np.inf, force=False):
         tpos2 = self.pos2 - np.atleast_2d(self.disp)
-        disps = [self.pos1[p[:,None],:] - tpos2[p[None,:],:] 
+        disps = [self.pos1[p[:,None],:] - tpos2[p[None,:],:]
                  for p in self.perm]
         for d in disps:
             d -= np.round(d/self.boxvec) * self.boxvec
@@ -175,7 +175,7 @@ class BranchNodeBulk(BranchNode):
             upperbound = sqrt(sum(d.min(0).sum() for d in dists))
         else:
             upperbound = 0.
-            
+
         if upperbound < bestupper or force:
             self.quick = False
             lowerperm = [lap(ld)[0] for ld in ldists]
@@ -193,10 +193,10 @@ class BranchNodeBulk(BranchNode):
             self.lowerbound = sqrt(sum(d.min(0).sum() for d in ldists))
             upperperm = None
         return self.lowerbound, self.upperbound, upperperm
-                 
+
 class BranchNodeCluster(BranchNode):
     branchvects = np.array(list(product([1,-1],repeat=3)))
-    def __init__(self, rot, width, pos1, pos2, 
+    def __init__(self, rot, width, pos1, pos2,
                  r1, r2, r1tr2, r1pr2, pos1tree):
         self.rot = rot
         self.width = width
@@ -207,7 +207,7 @@ class BranchNodeCluster(BranchNode):
         self.r1pr2 = r1pr2
         self.pos1tree = pos1tree
         self.n = 2
-    
+
     def branch(self):
         n = self.n
         n = 2
@@ -216,36 +216,36 @@ class BranchNodeCluster(BranchNode):
         newrots -= (n-1.)
         newrots *= 0.5 * self.width / n
         newrots += self.rot[None,:]
-        newnodes = [BranchNodeCluster(rot, self.width/n, 
-                               self.pos1, self.pos2, self.r1, self.r2, 
+        newnodes = [BranchNodeCluster(rot, self.width/n,
+                               self.pos1, self.pos2, self.r1, self.r2,
                                self.r1tr2, self.r1pr2, self.pos1tree)
                     for rot in newrots]
         return newnodes
-    
+
     def calcbounds(self, bestupper=np.inf, force=False):
         rpos2 = self.pos2.dot(angle_axis2mat(self.rot))
-        
+
         if not force:
             d, p = self.pos1tree.query(rpos2)
             upperbound = norm(d)
         else:
             upperbound = 0
-            
+
         if upperbound < bestupper or force:
             self.quick = False
             dists = ((self.pos1[:,None,:]-rpos2[None,:,:])**2).sum(2)
-        
+
             cosa = 0.5*(self.r1pr2 - dists)/self.r1tr2
             sina = sqrt(np.clip(1.-cosa**2,0.,1.))
             theta = min((sqrt(3) * self.width / 2),np.pi)
             cosd = cos(theta)
             sind = abs(sin(theta))
             cosdm = np.where(cosa>cosd, 1., cosa*cosd + sina*sind)
-            
+
             lowerbounddists = np.clip(self.r1pr2 - 2*self.r1tr2*cosdm, 0., np.inf)
             lowerperm = lap(lowerbounddists.copy())[0]
             self.lowerbound = sqrt(sum(lowerbounddists[i,j] for i,j in enumerate(lowerperm)))
-            
+
             if self.lowerbound < bestupper:
                 upperperm = lap(dists)[0]
                 self.upperbound = norm(self.pos1 - rpos2[upperperm])
@@ -265,12 +265,12 @@ class BranchNodeCluster(BranchNode):
             self.upperbound = upperbound
             self.lowerbound = lowerbound
             upperperm = p
-        
+
         self.rpos2 = rpos2
         return self.lowerbound, self.upperbound, upperperm
 
 class BranchnBoundAlignment(object):
-    
+
     def __init__(self,invert=True,boxSize=None):
         self.invert = invert
         self.libbnb = libbnb
@@ -301,15 +301,15 @@ class BranchnBoundAlignment(object):
         elif len(pos1) != self.Natoms:
             self.Natoms = len(pos1)
             self.setPerm([np.arange(self.Natoms)])
-            
+
         self.coordsb = np.array(pos1).flatten()
         self.coordsa = np.array(pos2).flatten()
         self.gopermdist.initialise(
-            self.coordsb, self.coordsa, self.boxvec[0], self.boxvec[1], 
+            self.coordsb, self.coordsa, self.boxvec[0], self.boxvec[1],
             self.boxvec[2], self.bulk)
         self.gopermdist.debug=debug
-            
-    def __call__(self, pos1, pos2, perm=None, invert=None, debug=False, 
+
+    def __call__(self, pos1, pos2, perm=None, invert=None, debug=False,
                  force=False, niter=1000, iprint=1):
         if invert is None:
             invert = self.invert
@@ -340,64 +340,62 @@ class BranchnBoundAlignment(object):
         else:
             rmat = self.gopermdist.bestrmat[:,:,bestid]
             return bestupper.item(), coordsb, coordsa, rmat
-        
+
 if __name__ == "__main__":
     import os
     import csv
 
     # Turn debug on if you want status messages
-    # You will get A LOT of print statements!    
+    # You will get A LOT of print statements!
     debug=False
-        
-    
+
+
     def readFile(filename):
         with open(filename, 'rb') as f:
             reader = csv.reader(f, delimiter=' ')
             dist = [map(float, row) for row in reader]
         return np.array(dist)
-    
+
     datafolder = "../examples/LJ38"
     natoms = 38
     pos1 = readFile(os.path.join(datafolder, 'coords'))
     pos2 = readFile(os.path.join(datafolder, 'finish'))
-    
-    
-    
-    bnbpy = BranchandBoundMaster()
-    
 
-    raise
-        
+
+
+    bnbpy = BranchandBoundMaster()
+
+
     dpyclus, coordsb, coordsa = bnbpy(pos1, pos2)
     fig, axes = bnbpy.plot()
-    
+
     if f90.have_fortran:
         bnbcluster = BranchnBoundAlignment()
-        dcluster, coordsb, coordsa, rmat = bnbcluster(pos1, pos2, debug=False, niter=10000)
-           
-    if f90.have_fortran:                  
+        dcluster, coordsb, coordsa, rmat = bnbcluster(pos1, pos2, debug=False, niter=1000)
+
+    if f90.have_fortran:
         datafolder = "../examples/BLJ256"
-    
+
         pos1 = readFile(os.path.join(datafolder, 'coords'))
         pos2 = readFile(os.path.join(datafolder, 'finish'))
-    
+
         natoms = 256
         ntypeA = 204
         shape = (natoms, 3)
         boxSize = np.ones(3)*5.975206329
         permlist = [np.arange(ntypeA), np.arange(ntypeA, natoms)]
         bnbbulk = BranchnBoundAlignment(invert=False, boxSize=boxSize)
-        
+
         # Testing for octahderal symetries will take ~48 times longer!
         dbulk, coordsab, coordsa = bnbbulk(pos1, pos2, debug=False, niter=10000)
-                              
+
     print 'Summary:'
     print 'Cluster alignment:'
-    print 'On example LJ38 data, distance should = 1.4767'  
-    print 'Branch and bound alignment: ', dpyclus    
-    if f90.have_fortran:         
+    print 'On example LJ38 data, distance should = 1.4767'
+    print 'Branch and bound alignment: ', dpyclus
+    if f90.have_fortran:
         print 'Branch and bound alignment: ', dcluster
-               
+
         print '\nPeriodic alignment:'
-        print 'On example BLJ256 data, distance should = 1.559'                            
+        print 'On example BLJ256 data, distance should = 1.559'
         print 'Branch and bound alignment: ', dbulk
